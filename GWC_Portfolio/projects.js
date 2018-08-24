44f9c1522a77b4d3856a56ff7a0f22ac03c64c80
annewang1235 <annewang3535@gmail.com>
2018-08-24 11:38:17 -0700

    Add files from GWC Portfolio

---
+++ GWC_Portfolio/projects.js
@@ -0,0 +1,83 @@
+var modal1 = document.getElementById('myModal1');
+
+var btn_1 = document.getElementById("myBtn_1");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn_1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+span.onclick = function() {
+    modal1.style.display = "none";
+}
+
+var modal6 = document.getElementById('myModal6');
+
+var btn_6 = document.getElementById("myBtn_6");
+
+var span = document.getElementsByClassName("close")[1];
+
+btn_6.onclick = function() {
+    modal6.style.display = "block";
+}
+
+span.onclick = function() {
+    modal6.style.display = "none";
+}
+
+var modal2 = document.getElementById('myModal2');
+
+var btn_2 = document.getElementById("myBtn_2");
+
+var span = document.getElementsByClassName("close")[2];
+
+btn_2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+span.onclick = function() {
+    modal2.style.display = "none";
+}
+
+var modal5 = document.getElementById('myModal5');
+
+var btn_5 = document.getElementById("myBtn_5");
+
+var span = document.getElementsByClassName("close")[3];
+
+btn_5.onclick = function() {
+    modal5.style.display = "block";
+}
+
+span.onclick = function() {
+    modal5.style.display = "none";
+}
+
+var modal3 = document.getElementById('myModal3');
+
+var btn_3 = document.getElementById("myBtn_3");
+
+var span = document.getElementsByClassName("close")[4];
+
+btn_3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+span.onclick = function() {
+    modal3.style.display = "none";
+}
+
+var modal4 = document.getElementById('myModal4');
+
+var btn_4 = document.getElementById("myBtn_4");
+
+var span = document.getElementsByClassName("close")[5];
+
+btn_4.onclick = function() {
+    modal4.style.display = "block";
+}
+
+span.onclick = function() {
+    modal4.style.display = "none";
+}
